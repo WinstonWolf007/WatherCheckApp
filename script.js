2c32198f511d827eaa2805ec35f9dea1a9369be7
WinstonWolf007 <74386536+WinstonWolf007@users.noreply.github.com>
2022-03-06 15:13:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ function openPage(name) {
     page1.style.display = 'none';
     page2.style.display = 'none';
     page3.style.display = 'none';
-
+   
     title.innerHTML = name.toUpperCase();
+
     document.getElementById(name+'-page').style.display = 'block';
-
-
     document.getElementById(name+'-page').style.animation = 'sympa 1s';
 }
+
+
+
+let theme_3d_change = false;
+let nav = document.querySelector('#settings-page #el3 input');
+let el1 = document.querySelector('.nav .btn-nav');
+nav.addEventListener('click', function() {
+    alert('click')
+    if (theme_3d_change) {
+        nav2.classList.add('logo');
+        nav2.classList.add('logo');
+        nav2.classList.add('logo');
+        theme_3d_change = false;
+        // window.location.reload();
+    }
+    else {
+        nav2.classList.remove('logo');
+        nav2.classList.remove('logo');
+        nav2.classList.remove('logo');
+        theme_3d_change = true;
+        // window.location.reload();
+    }
+})
